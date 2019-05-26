1a1349799e1a5f596d7af235067ec7219b25bdb0
Santiago Fernández González <santiagofdezglez@gmail.com>
2019-05-26 12:32:15 +0200

    Implement create and delete index
    -Implement class Connection
    
    -Define operations with documents

--- query.py
+++ query.py
@@ -8,24 +8,55 @@ Author: Santiago Fernández González
 License: MIT
 """
 
+from elasticsearch import Elasticsearch, TransportError
+from elasticsearch_dsl import Search, A
+
 
 class Connection:
+    """
+    Return a connection with the Elasticsearch host
+    """
 
     @staticmethod
     def get_connection():
         # Return a Elasticsearch instance
-        pass
+        return Elasticsearch([{'host': 'localhost', 'port': 9200}])
 
 
 class Index:
+    """
+    Manage basic operations over an index, such as create and remove an index,
+    add and delete documents from that index, etc.
+    """
+    name = None
+    connection = None
 
-    def create(self):
-        # If it's already created => don't throw exception
-        # Return 0 or 1
+    @staticmethod
+    def create(connection, index_name, body=None):
+        # If it's already created => return None
+        try:
+            if body is None:
+                connection.indices.create(index=index_name)
+            else:
+                connection.indices.create(index=index_name, body=body)
+            instance = Index()
+            instance.name = index_name
+            instance.connection = connection
+            return instance
+
+        except TransportError:
+            return None
+
+    def delete(self):
+        self.connection.indices.delete(self.name)
+
+    def index_doc(self, body):
         pass
 
-    def remove(self):
-        # Return 0 or 1
+    def get_doc(self, id):
+        pass
+
+    def delete_doc(self, id):
         pass
 
 
