7ae74778b014fb2b36e91b2bdbd451069d4623f8
Santiago Fernández González <santiagofdezglez@gmail.com>
2019-05-29 00:34:40 +0200

    Add (again) search_by_id to Search

--- search.py
+++ search.py
@@ -126,6 +126,8 @@ class Search:
         # Return only documents with score > 0
         return response.hits
 
+    def search_by_id(self, id):
+        return Document.get(id, self.__es, self.__index)
 
 if __name__ == '__main__':
     """
